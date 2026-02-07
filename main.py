8d6a9e32e703edcaffc5839bc03478453e74bc65
Quantum Cog <thequantumcog@protonmail.com>
2026-02-07 13:12:12 +0500

    Initial Integration

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 """
 SDA Project Phase 1 - Data Loading and Processing
 """
-
+from data_loader import reshape_to_long_format, load_csv
+from data_processor import clean_dataframe
 import config_parser
 import pandas as pd
 import filter
@@ -18,17 +19,28 @@ def print_section(title: str) -> None:
 
 
 def main():
+    filepath = "gdp_with_continent_filled.csv"
+    try:
+        config_array = config_parser.get_config_options()
+        print_section("SDA PROJECT PHASE 1 - Data Loading & Processing")
 
-    config_array = config_parser.get_config_options()
-    print_section("SDA PROJECT PHASE 1 - Data Loading & Processing")
+        df = load_csv(filepath)  # file read
+        long_data = reshape_to_long_format(df)
 
-    #  Load Data
+        df_clean = clean_dataframe(
+            long_data,
+            handle_missing=True,
+            missing_strategy='mean',  # Using mean strategy
+            remove_duplicates=True
+        )
 
-    # Step 3: Extract Metadata
+        filtered_data = filter.data(df_clean, config_array)
+        print(filtered_data)
 
-    df = pd.read_csv('temp_test.csv')
-    filtered_data = filter.data(df, config_array)
-    print(filtered_data)
+    except FileNotFoundError as e:
+        print(f"\n✗ Error: {e}")
+    except Exception as e:
+        print(f"\n✗ Unexpected error: {e}")
 
 
 if __name__ == "__main__":
